cdc32534a6c94ccbd2669bc99d51198b9a532205
wuuwuu26 <2522770402@qq.com>
2026-02-10 11:58:21 +0800

    Delete Elastic-Tracker/devel directory

--- Elastic-Tracker/devel/lib/python3/dist-packages/object_detection_msgs/msg/__init__.py
+++
@@ -1,5 +0,0 @@
-from ._BoundingBox import *
-from ._BoundingBoxes import *
-from ._CarPosition import *
-from ._ObjectCount import *
-from ._motorAngle import *
--- Elastic-Tracker/devel/share/common-lisp/ros/object_detection_msgs/msg/_package_motorAngle.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package object_detection_msgs-msg)
-(cl:export '(HEADER-VAL
-          HEADER
-          ANGLE-VAL
-          ANGLE
-))
